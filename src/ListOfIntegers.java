4f134ac808e5cbd69b0b81076287c503cc5dcf66
sruthi <sruthi49@gmail.com>
2022-12-05 15:12:05 -0700

    Program added

--- src/ListOfIntegers.java
+++ src/ListOfIntegers.java
@@ -14,22 +14,31 @@ public class ListOfIntegers {
         list1.add(1); 
         list1.add(2); 
         list1.add(6); 
-        ArrayList<Integer> expected = new ArrayList<Integer>();
-		 expected .add(4);
+        ArrayList<Integer> expected1 = new ArrayList<Integer>();
+		 expected1 .add(4);
 		 //expected.add(6);
-		Assert.assertEquals(expected,two2(list1)); 
+		Assert.assertEquals(expected1,two2(list1)); 
+		ArrayList<Integer> list2 = new ArrayList<Integer>();
+        list2.add(1); 
+        list2.add(100); 
+        list2.add(202); 
+        ArrayList<Integer> expected2 = new ArrayList<Integer>();
+		 expected2 .add(200);
+		 expected2.add(404);
+		Assert.assertEquals(expected2,two2(list2)); 
+		//1,100,202
 	}
 
-	public List<Integer> two2(ArrayList<Integer> list2) {
+	public List<Integer> two2(ArrayList<Integer> arr) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		
-		for(int i=0;i<list2.size();i++)
+		for(int i=0;i<arr.size();i++)
 		{
 			
-			int num = list2.get(i)*2;
-			if(num%10 == 2)
+			int num = arr.get(i)*2;
+			if(num%10 == 2 || num%100 == 2)
 			{
-				list2.remove(i);
+				arr.remove(i);
 				i--;
 			}
 			else 
